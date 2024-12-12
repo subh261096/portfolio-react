88e46c9c140a783cc3d5b66af5c3a8618d134f4c
subhashish Singh <51812041+subh261096@users.noreply.github.com>
2024-12-12 21:36:20 +0000

    created elem and root rendered to html

--- parcel_react/index.js
+++ parcel_react/index.js
@@ -1,1 +1,6 @@
-console.log("hi");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const elem = React.createElement("h1", null, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(elem);
