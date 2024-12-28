bca154148f202e500c58e4352371c10ecff759f3
subhashish Singh <51812041+subh261096@users.noreply.github.com>
2024-12-28 10:08:48 +0000

    seperate stateless component

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react'
 
+
+const Joke = ({ joke: { setup, punchline}}) =>{console.log(setup,punchline);
+  return (
+  <p style={{margin: 20}}>{setup} <em>{punchline}</em></p>
+)}
+
 export default class Jokes extends Component {
-  state = {json: {} }
+  state = {joke: {}, ten_jokes: []};
 
   componentDidMount() {
     fetch("https://official-joke-api.appspot.com/jokes/programming/random")
     .then(response => response.json())
-    .then(data => this.setState({json: data[0]}))
+    .then(data => this.setState({joke: data[0]}))
+  };
+
+  fetchTenJokes = () => {
+    fetch("https://official-joke-api.appspot.com/jokes/programming/ten")
+    .then(response => response.json())
+    .then(data => this.setState({ten_jokes: data}))
   };
   
   render() {
-    const {setup, punchline} = this.state.json;
     return (
       <>
         <h2>Highlighted Joke</h2>
-        <div>{setup} {punchline}</div>
+        <Joke joke={this.state.joke} />
+        <hr />
+        <h3>10 more jokes</h3>
+        <button onClick={this.fetchTenJokes} type='submit'>Fetch jokes!</button>
+        {this.state.ten_jokes.map(joke => {
+          <Joke key={joke.id} joke={joke} />
+        })}
       </>
     )
   }
